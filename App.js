a1e6b5fd536ba040f5cdc0e103694f16ca029a52
marius93rm <marius93rm@gmail.com>
2022-11-03 10:18:06 +0100

    updated with debug button

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React, { useContext, createContext, useEffect, useState } from 'react';
-import { Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const CounterContext = createContext(0);
 
-const useCounter = () => {
-  useContext(CounterContext);
-};
-
 const CounterProvider = ({ children }) => {
   const [counter, setCounter] = useState(0);
 
+  const test = () => {
+    Alert.alert('test');
+  };
   const increment = () => setCounter((c) => c + 1);
   const decrement = () => setCounter((c) => c - 1);
 
@@ -27,23 +26,32 @@ const CounterProvider = ({ children }) => {
   }, []); // <-- empty array means "run this only once"
 
   return (
-    <CounterContext.Provider value={{ counter, increment, decrement }}>
+    <CounterContext.Provider value={{ counter, increment, decrement, test }}>
       {children}
     </CounterContext.Provider>
   );
 }
 
-
 export default function App() {
-  const { counter, increment, decrement } = useCounter();
+  const { counter, increment, decrement, test } = useContext(CounterContext);
 
   return (
     <CounterProvider>
-      <View>
-        <Text>{counter}</Text>
+      <View style={styles.container}>
+        <Text>{counter} -</Text>
         <Button title="Increment" onPress={increment} />
         <Button title="Decrement" onPress={decrement} />
+        <Button title="Test" onPress={test} />
       </View>
     </CounterProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems : 'center',
+    justifyContent: 'center',
+  },
+});
